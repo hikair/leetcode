2e08d8bd17fb18b5b1392aa1bb76d762a03fb2cf
zpq <351477856@qq.com>
2021-08-29 21:55:17 +0800

    所有奇数长度子数组的和

---
+++ src/test/java/com/arr/SumOddLengthSubarraysTest.java
@@ -0,0 +1,26 @@
+package com.arr;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SumOddLengthSubarraysTest extends TestCase {
+
+    SumOddLengthSubarrays obj;
+
+    @Before
+    public void init() {
+        obj = new SumOddLengthSubarrays();
+    }
+
+    @Test
+    public void testSumOddLengthSubarrays() {
+        Assert.assertEquals(obj.sumOddLengthSubarrays(new int[]{1, 4, 2, 5, 3}), 58);
+        Assert.assertEquals(obj.sumOddLengthSubarrays(new int[]{1, 2}), 3);
+        Assert.assertEquals(obj.sumOddLengthSubarrays(new int[]{10, 11, 12}), 66);
+    }
+}
