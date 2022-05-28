47490b962343ac646f7403211a13199055c2a8cb
PengQian <351477856@qq.com>
2022-05-28 09:13:38 +0800

    全排列

---
+++ src/main/java/com/backtrack/middle/Permute.java
@@ -0,0 +1,64 @@
+package com.backtrack.middle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,2,3]
+ * 输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ * 示例 2：
+ *
+ * 输入：nums = [0,1]
+ * 输出：[[0,1],[1,0]]
+ * 示例 3：
+ *
+ * 输入：nums = [1]
+ * 输出：[[1]]
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 6
+ * -10 <= nums[i] <= 10
+ * nums 中的所有整数 互不相同
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/permutations
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Permute {
+
+    List<List<Integer>> result = new ArrayList<>();
+
+    List<Integer> path = new ArrayList<>();
+
+    boolean[] used;
+
+    public List<List<Integer>> permute(int[] nums) {
+        used = new boolean[nums.length];
+        backtracking(nums);
+        return result;
+    }
+
+    public void backtracking(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            path.add(nums[i]);
+            used[i] = true;
+            if (path.size() == nums.length) {
+                result.add(new ArrayList<>(path));
+            }
+            backtracking(nums);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+}
---
+++ src/test/java/com/backtrack/middle/PermuteTest.java
@@ -0,0 +1,14 @@
+package com.backtrack.middle;
+
+import org.junit.Test;
+
+
+public class PermuteTest {
+
+    Permute obj = new Permute();
+
+    @Test
+    public void permute() {
+        System.out.println(obj.permute(new int[]{1, 2, 3}));
+    }
+}
