51a28d36aa544328200954c31f076b9a3671fca1
PengQian <351477856@qq.com>
2022-06-16 07:53:26 +0800

    数组中的 k-diff 数对

---
+++ src/test/java/com/binarysearch/middle/FindPairsTest.java
@@ -0,0 +1,18 @@
+package com.binarysearch.middle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class FindPairsTest {
+
+    FindPairs obj = new FindPairs();
+
+    @Test
+    public void findPairs() {
+        Assert.assertEquals(2, obj.findPairs(new int[]{3, 1, 4, 1, 5}, 2));
+        Assert.assertEquals(4, obj.findPairs(new int[]{1, 2, 3, 4, 5}, 1));
+        Assert.assertEquals(1, obj.findPairs(new int[]{1, 3, 1, 5, 4}, 0));
+        Assert.assertEquals(2, obj.findPairs(new int[]{1, 1, 1, 2, 2}, 0));
+    }
+}
