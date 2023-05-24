48e7a2b76e7a53ed6b20b4cb0662c50738b6cdc7
zpq <351477856@qq.com>
2023-05-24 19:19:16 +0800

    交错字符串

--- src/main/java/com/dp/middle/IsInterleave.java
+++ src/main/java/com/dp/middle/IsInterleave.java
@@ -45,6 +45,31 @@ package com.dp.middle;
  */
 public class IsInterleave {
 
+    /**
+     * 动态规划
+     * s1 = bacc, s2 = aabcce, s3 = abaacbccec
+     * 打表
+     *     | 0 | a | a | b | c | c | e |
+     * | 0 | T | T | F | F | F | F | F |
+     * | b | F | T | T | F | F | F | F |
+     * | a | F | T | T | F | F | F | F |
+     * | c | F | F | T | T | T | T | T |
+     * | c | F | F | F | F | T | F | T |
+     * 行（i）表示s1的第i个元素
+     * 列（j）表示s2的第j个元素
+     * dp[i][j] 表示 s1前i个元素与s2前j个元素加在一起是否可以表示s3前i + j个元素
+     * 边界：
+     * 1.dp[0][0] = true
+     * 2.dp[i][0] = s1前i个元素是否是s3前i个元素
+     * 3.dp[0][j] = s2前j个元素是否是s3前j个元素
+     * 转移方程：
+     * dp[i][j] = (dp[i - 1][j] && (s1第i个元素是否是s3当前长度的最后一个元素)) or (dp[i][j - 1] && (s2第j个元素是否是s3当前长度的最后一个元素))
+     *
+     * @param s1
+     * @param s2
+     * @param s3
+     * @return
+     */
     public boolean isInterleave(String s1, String s2, String s3) {
         int len1 = s1.length(), len2 = s2.length(), len3 = s3.length();
         if (len1 + len2 != len3) {
